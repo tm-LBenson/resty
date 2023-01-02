124d860706f6e64c5277a07a6386b7024cb5fb25
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-02 16:15:44 -0500

    convert Header to RFC

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,13 @@
+/** @format */
+
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  );
 }
 
 export default Header;

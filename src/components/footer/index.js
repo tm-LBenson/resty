574863aba9f879409ed9364be1d8412b9e90724e
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-02 16:13:14 -0500

    convert footer to RFC

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,9 @@
+/** @format */
+
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+function Footer() {
+  return <footer>&copy; 2018</footer>;
 }
 
 export default Footer;

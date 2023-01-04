fff6803e0fad25619039b2d5ebf26d105020476c
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-04 17:59:58 -0500

    add section wrapper for styling add body to state

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -10,58 +10,69 @@ function Form({ handleApiCall }) {
     const formData = {
       method: method,
       url: e.target.url.value,
+      body: e.target.json.value,
     };
     await handleApiCall(formData);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>URL: </span>
-          <input
-            data-testid="form-input"
-            name="url"
-            type="text"
-          />
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>URL: </span>
+            <input
+              data-testid="form-input"
+              name="url"
+              type="text"
+            />
 
-          <button data-testid="form-button" type="submit">GO!</button>
-        </label>
-        <label
-          onClick={(e) => {
-            setMethod(e.target.id);
-          }}
-          className="methods"
-        >
-          <span
-            className={method === 'get' ? 'selected' : 'not'}
-            id="get"
+            <button
+              data-testid="form-button"
+              type="submit"
+            >
+              GO!
+            </button>
+          </label>
+          <label
+            onClick={(e) => {
+              setMethod(e.target.id);
+            }}
+            className="methods"
           >
-            GET
-          </span>
-          <span
-            className={method === 'post' ? 'selected' : 'not'}
-            id="post"
-          >
-            POST
-          </span>
-          <span
-            className={method === 'put' ? 'selected' : 'not'}
-            id="put"
-          >
-            PUT
-          </span>
-          <span
-            className={method === 'delete' ? 'selected' : 'not'}
-            id="delete"
-          >
-            DELETE
-          </span>
-        </label>
-        <label>
-          <textarea/>
-        </label>
-      </form>
+            <span
+              className={method === 'get' ? 'selected' : 'not'}
+              id="get"
+            >
+              GET
+            </span>
+            <span
+              className={method === 'post' ? 'selected' : 'not'}
+              id="post"
+            >
+              POST
+            </span>
+            <span
+              className={method === 'put' ? 'selected' : 'not'}
+              id="put"
+            >
+              PUT
+            </span>
+            <span
+              className={method === 'delete' ? 'selected' : 'not'}
+              id="delete"
+            >
+              DELETE
+            </span>
+          </label>
+          <label>
+            <textarea
+              name="json"
+              className="json"
+            />
+          </label>
+        </form>
+      </section>
     </>
   );
 }
